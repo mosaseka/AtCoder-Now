a1db863df9ba2536ea1e829936439c4067678d78
山本 凌久 <124500563+mosaseka@users.noreply.github.com>
2024-12-14 20:48:49 +0900

    Delete AtCoder Problems directory

--- AtCoder Problems/A - Jiro.py
+++
@@ -1,8 +0,0 @@
-AB, AC, BC = map(str, input().split())
-
-if AB != AC:
-  print("A")
-elif AB == BC:
-  print("B")
-else:
-  print("C")
--- AtCoder Problems/A - Seats.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-S = str(input())
-COUNT = 0
-
-for i in range(N-2):
-  if S[i] == "#" and S[i+2] == "#":
-    COUNT += 1
-
-print(COUNT)
